7b7db65b8fcc20293ac87936effe458257b0558e
Alex Asriyan <aasriyan01@gmail.com>
2020-09-14 11:42:00 -0400

    added DOM starting code module

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,26 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = (name) => console.log('Hi ' + name);
+const sayGreeting = (greeting, name = 'Bill') =>
+  console.log(`${greeting} ${name}`);
+const sayHelloHardcoded = () => console.log('Hello, this is hardcoded.');
+const sayHelloReturn = (name) => `Hello, ${name} from returned`;
 
-sayHello();
+const checkInput = (cb, ...args) => {
+  let isEmpty = false;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i]) {
+      continue;
+    } else {
+      isEmpty = true;
+      break;
+    }
+  }
+  if (isEmpty) cb();
+};
+
+const validString = () => {
+  console.log('There are no empty strings');
+};
+
+checkInput(validString, 'bb', '256', 'string');
+
+sayHello('Alex');
